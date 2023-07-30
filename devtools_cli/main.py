4a10e617ef885cfedc496b50d62f18bcbdd803d5
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-07-31 00:47:39 +0300

    Expanded main cli command versatility

--- devtools_cli/main.py
+++ devtools_cli/main.py
@@ -1,25 +1,13 @@
 #
-#  MIT License
+#   MIT License
 #
-#  Copyright (c) 2023 Mattias Aabmets
+#   Copyright (c) 2023, Mattias Aabmets
 #
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
+#   This file is subject to the terms and conditions defined in
+#   the License. You may not use, modify, or distribute this file
+#   except in compliance with the License.
 #
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
+#   SPDX-License-Identifier: MIT
 #
 import inspect
 import importlib
@@ -29,7 +17,7 @@ from pathlib import Path
 app = Typer()
 
 package_path = Path(__file__).parent
-import_dir = package_path / "sub_apps"
+import_dir = package_path / "commands"
 
 for filepath in import_dir.rglob("*.py"):
 	relative_path = filepath.relative_to(package_path)
@@ -38,11 +26,14 @@ for filepath in import_dir.rglob("*.py"):
 		package=package_path.name,
 		name=f'.{module_path}'
 	)
-
 	for _, obj in inspect.getmembers(module):
 		if isinstance(obj, Typer):
-			app.registered_commands.extend([
-				*obj.registered_commands
-			])
+			name = obj.info.name
+			if isinstance(name, str) and len(name) > 0:
+				app.add_typer(obj, name=name)
+			else:
+				app.registered_commands.extend([
+					*obj.registered_commands
+				])
 
 	importlib.invalidate_caches()
