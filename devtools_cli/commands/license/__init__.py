b7c92cc3ce30353c041bf92fbac30d82d9ba8997
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-08-01 11:35:44 +0300

    Updated source code license headers

--- devtools_cli/commands/license/__init__.py
+++ devtools_cli/commands/license/__init__.py
@@ -3,9 +3,8 @@
 #
 #   Copyright (c) 2023, Mattias Aabmets
 #
-#   This file is subject to the terms and conditions defined in
-#   the License. You may not use, modify, or distribute this file
-#   except in compliance with the License.
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
 #
 #   SPDX-License-Identifier: MIT
 #
