81ca461da622d109c774b0ca150b9c1821b0e5e3
Mattias Aabmets <mattias.aabmets@gmail.com>
2023-08-26 17:44:37 +0300

    Created helper funcs for the log command and added the "add" subcommand.

--- devtools_cli/commands/log/main.py
+++ devtools_cli/commands/log/main.py
@@ -8,27 +8,60 @@
 #
 #   SPDX-License-Identifier: MIT
 #
-from pathlib import Path
-from datetime import date
+from rich.prompt import Prompt
 from rich.console import Console
 from typer import Typer, Option
 from typing_extensions import Annotated
-from devtools_cli.utils import *
 from devtools_cli.commands.version.models import VersionConfig
+from devtools_cli.utils import *
+from .helpers import *
+from .errors import *
 
 
 app = Typer(name="log", help="Manages project changelog file.")
 console = Console(soft_wrap=True)
 
-CHANGELOG_FILENAME = "CHANGELOG.md"
-SECTION_LEVEL = '###'
-HEADER = [
-    "# Changelog",
-    "",
-    "All notable changes to this project will be documented in this file.  ",
-    "The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/), "
-    "and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).  "
-]
+
+NewSectionOpt = Annotated[bool, Option(
+    '--new', '-n', show_default=False, help=''
+    'If the changes should be added to a new version section.'
+)]
+
+
+@app.command(name="add", epilog="Example: devtools log add --new")
+def cmd_add(new_section: NewSectionOpt = False):
+    """
+    Interactively add changes into the changelog file.
+    """
+    config: VersionConfig = read_local_config_file(VersionConfig)
+    version = config.app_version
+
+    existing = read_existing_content(init_cwd=True)
+    if not existing and not new_section:
+        new_section = True
+    elif new_section and not validate_unique_version(version, existing):
+        console.print("ERROR! Cannot insert a duplicate version section into the changelog file.\n")
+        raise SystemExit()
+
+    console.print("Please provide the changelog contents: (Press Enter on empty prompt to apply.)")
+
+    changes = []
+    while True:
+        change = Prompt.ask("Entry")
+        if change == '':
+            break
+        changes.append(change)
+    conformed = conform_changes(changes)
+
+    if not changes:
+        console.print("Did not alter the changelog file.\n")
+    else:
+        console.print("Added the provided changes into the changelog file.\n")
+
+    if new_section:
+        write_new_section(version, conformed, existing)
+    else:
+        update_latest_section(conformed, existing)
 
 
 ChangesOpt = Annotated[str, Option(
@@ -37,35 +70,25 @@ ChangesOpt = Annotated[str, Option(
 )]
 
 
-@app.command(name="insert", epilog="Example: devtools log next --changes \"changes\"")
-def cmd_next(changes: ChangesOpt = ''):
-    config_file: Path = find_local_config_file(init_cwd=True)
-    logfile = config_file.parent / CHANGELOG_FILENAME
-
-    if not logfile.exists():
-        logfile.touch(exist_ok=True)
-        with logfile.open('w') as file:
-            file.write('\n'.join(HEADER))
-
-    with logfile.open('r') as file:
-        lines = file.read().splitlines()
-
-    existing = []
-    for i, line in enumerate(lines):
-        if line.startswith(SECTION_LEVEL):
-            existing = lines[i:]
-            break
-
+@app.command(name="insert", epilog="Example: devtools log insert --changes \"changes\"")
+def cmd_insert(changes: ChangesOpt = ''):
+    """
+    This command is intended to be used in a bash script to insert
+    variable contents into a new version section of a changelog file.
+    """
     config: VersionConfig = read_local_config_file(VersionConfig)
-    title = f"{SECTION_LEVEL} [{config.app_version}] - {date.today().isoformat()}  "
-    changes = [c if c.startswith("- ") else f"- {c}" for c in changes.splitlines()]
+    version = config.app_version
 
-    with logfile.open('w') as file:
-        contents = [*HEADER, '', title, '', *changes, '', *existing]
-        file.write('\n'.join(contents))
+    existing = read_existing_content(init_cwd=True)
+    if not validate_unique_version(version, existing):
+        console.print("ERROR! Cannot insert a duplicate version section into the changelog file.\n")
+        raise SystemExit()
+
+    conformed = conform_changes(changes)
+    write_new_section(version, conformed, existing)
 
     verb = "updated" if existing else "created"
-    console.print(f"Successfully {verb} the {CHANGELOG_FILENAME} file.")
+    console.print(f"Successfully {verb} the changelog file.")
 
 
 VersionOpt = Annotated[str, Option(
@@ -76,38 +99,40 @@ VersionOpt = Annotated[str, Option(
 
 @app.command(name="view", epilog="Example: devtools log view --version 1.2.3")
 def cmd_view(version: VersionOpt = None):
-    config_file = find_local_config_file(init_cwd=False)
-    if config_file is None:
-        console.print("ERROR! Project is not initialized with a devtools config file!")
+    try:
+        existing = read_existing_content(init_cwd=False)
+        if not existing:
+            console.print("ERROR! The changelog does not contain any entries.\n")
+            raise SystemExit()
+    except ConfigFileNotFound:
+        console.print("ERROR! Project is not initialized with a devtools config file.\n")
+        raise SystemExit()
+    except ChangelogFileNotFound:
+        console.print("ERROR! Cannot view sections of a non-existent CHANGELOG.md file.\n")
         raise SystemExit()
 
-    logfile = config_file.parent / CHANGELOG_FILENAME
-    if not logfile.exists():
-        console.print("ERROR! Cannot view sections of a non-existent CHANGELOG.md file!")
-        raise SystemExit()
-
-    with logfile.open('r') as file:
-        lines = file.read().splitlines()
+    label = SECTION_LEVEL
+    if version:
+        label += f" [{version}]"
 
     line: str
-    section = f"{SECTION_LEVEL} [{version}]" if version else f"{SECTION_LEVEL}"
-    for i, line in enumerate(lines):
-        if line.startswith(section):
-            end = len(lines)
+    for i, line in enumerate(existing):
+        if line.startswith(label):
+            end = len(existing)
             for j in range(i + 1, end):
-                if lines[j].startswith(f"{SECTION_LEVEL}"):
+                if existing[j].startswith(f"{SECTION_LEVEL}"):
                     end = j
                     break
 
             ver_type = 'Version' if version else "Latest version"
-            ver_ident = version or line.split('[')[-1].split(']')[0]
+            ver_ident = extract_version_from_label(line)
             print(f"{ver_type} {ver_ident} changelog:")
 
-            contents = lines[i + 2:end]
+            contents = existing[i + 2:end]
             for c in contents:
                 print(c)
             if contents[-1] != '':
                 print('')
             return
 
-    console.print(f"The changelog does not contain a section for version {version}.")
+    console.print(f"The changelog does not contain any sections for version {version}.")
